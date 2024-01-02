542cf37dc2ecad5cbb532ee3c83540ca0276f01c
Mahmoud Khaled <78917638+mahmoudzkhaledd@users.noreply.github.com>
2024-01-02 10:26:45 +0200

    2000

--- Features/Core/Orders/Validators/OrderValidator.js
+++ Features/Core/Orders/Validators/OrderValidator.js
@@ -2,7 +2,7 @@ const validator = require('../../../../middlewares/validatorMiddleware');
 const { check } = require('express-validator');
 
 module.exports.validatorOrderService = [
-    check('notes').isString().isLength({ min: 0, max: 200 }).withMessage("notes must be between 0 and 200 characters")
+    check('notes').isString().isLength({ min: 0, max: 2000 }).withMessage("notes must be between 0 and 200 characters")
         .optional(),
     
     validator,
